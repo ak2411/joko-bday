f67d79ff63b7801e5929ac849879534ce5f61d41
Angelica Kosasih <angelicakosasih24@gmail.com>
2022-03-19 09:30:09 -0400

    Mobile compatible

--- src/Message.js
+++ src/Message.js
@@ -6,14 +6,26 @@ function Message() {
     return (
         <div className="message">
         <div className="message-container">
-                <div className="message-content">
+            <div className="message-content image">
                 <Image src={cicididi} className="message-img"/>
-                </div>                
-                <div className="message-content text">To the sweetest dad in the world, thank you for:
-believing we can do whatever we set our mind to do
-challenging us to do better
-loving us nonetheless
-having a soft spot for ur super cute si lucu si cantik daughters</div>
+             </div>                
+            <div className="message-content text">To the sweetest dad in the world, thank you for: 
+                <br /><br/>
+                1. Believing we can do whatever we set our minds to <br/>
+                2. Always challenging us to do and be better <br/>
+                3. Being our number one supporter <br/>
+                4. Being the best role model we could ever ask for <br/>
+                5. Always being there for us <br/>
+                6. Always letting us know how much you love us (We love you too) <br/>
+                <br/><br/>
+                We are forever grateful that you are our dad. From the moment we had our first breath till now, there has never been a single moment when you were not there. You are always selfless. You are always dependable. You are always caring. You are always putting the family first. You are always there. You are our rock. We love you so much and are so grateful for everything you do. <br/> 
+                <br/><br/><br/>
+
+                Happiest birthday Daddy! We love and miss you. 
+                <br/><br/><br/>
+
+                Cici and Didi
+            </div>
         </div>
     </div>
     );
